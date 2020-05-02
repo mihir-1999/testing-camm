5b993c855b25cca6e4add7794618bb1dd4a8c160
Mihir Narayan <mnaryan@iitr.ac.com>
2020-05-02 06:17:21 +0530

    final app page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import LogIn from './Pages/LogIn';
+import CustomerDetails from './Pages/CustomerDetails';
+import Selfie from './Pages/Selfie';
+import GovtID from  './Pages/GovtID';
+import Success from './Pages/Success';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+    super();
+        console.log('AppConstructor');
+    }
+        state={
+        pages:[
+          
+          {
+            id:0,
+            address:"/",
+            name: LogIn,
+            nextPage:"/customerdetails"
+          },
+          {
+            id:1,
+            address:"/customerdetails",
+            name: CustomerDetails,
+            nextPage:"/selfie"
+          },
+          {
+            id:2,
+            address:"/selfie",
+            name: Selfie,
+            nextPage:"/govtid"
+          },
+          {
+            id:3,
+            address: "/govtid",
+            name:GovtID,
+            nextPage:"/success"
+          },
+          {
+            id:4,
+            address: "/success",
+            name: Success,
+            nextPage:"/"
+          }
+        ]
+      }
+
+    componentWillMount(){
+      console.log('AppComponentDidMount');
+      this.state.pages.map((page)=>{
+        return(
+          localStorage.setItem(page.address,page.nextPage)
+        )
+      })
+    }
+  
+    
+    
+    render(){
+      console.log('AppRender');
+      return(
+        <Router>
+        {
+          this.state.pages.map((page)=>{
+            return(
+              <Route exact strict path={page.address} component={page.name} key={page.id}></Route>
+            )
+          })
+        }
+        </Router>
+      );
+    }
+
 }
 
 export default App;
