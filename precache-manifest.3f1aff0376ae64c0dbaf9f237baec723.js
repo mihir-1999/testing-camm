e8b44ff38472af34256c58b50e66b010f8606103
Mihir Narayan <mnaryan@iitr.ac.com>
2020-05-02 05:34:57 +0530

    Updates

---
+++ precache-manifest.3f1aff0376ae64c0dbaf9f237baec723.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b9414f2be70ab28aa59728de4508c9",
+    "url": "/testing-camm/index.html"
+  },
+  {
+    "revision": "32dbb04dfc1ab614f003",
+    "url": "/testing-camm/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ce0e84ab66e064062b53",
+    "url": "/testing-camm/static/js/2.91e79346.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testing-camm/static/js/2.91e79346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32dbb04dfc1ab614f003",
+    "url": "/testing-camm/static/js/main.7347f3bc.chunk.js"
+  },
+  {
+    "revision": "c3a62ad67783741fbf70",
+    "url": "/testing-camm/static/js/runtime-main.08efd851.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing-camm/static/media/logo.5d5d9eef.svg"
+  }
+]);
